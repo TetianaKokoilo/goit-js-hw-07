880facb70dc360c962aba718b308a49b1aa9a0f4
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2022-12-20 21:44:13 +0200

    add

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,33 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const imageGallery  = document.querySelector('.gallery');
+
+
+
+function createNewImageGallery() {
+    
+    const markup = galleryItems.map(item => 
+        `<div class="gallery__item">
+        <a href="${item.original}" class="gallery__link">
+          <img class="gallery__image"
+          scr ="${item.preview}" 
+          data-source = "${item.original}" 
+          alt="${item.description}">
+        </a>
+      </div>`).join('')
+    
+    imageGallery.insertAdjacentHTML('beforeend', markup)
+   
+}
+createNewImageGallery()
+console.log(imageGallery);
+
+imageGallery.addEventListener('click', onClick);
+function onClick(event) {
+    preventDefault()
+    console.dir(event.target)
+    
+}
+

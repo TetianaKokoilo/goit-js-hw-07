2fc91826912c2875246528d2e863b42b21ca966b
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2022-12-21 18:11:18 +0200

    Update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,43 +5,50 @@ console.log(galleryItems);
 
 const imageGallery = document.querySelector('.gallery');
 
-  const markup = galleryItems
-    .map(
-      item =>
-        `<div class="gallery__item">
+const markup = galleryItems
+  .map(
+    item =>
+      `<div class="gallery__item">
             <a class="gallery__link" href="${item.original}">
                 <img class="gallery__image"
                 src ="${item.preview}" 
                 data-source = "${item.original}" 
                 alt="${item.description}"/>
             </a>
-        </div>`).join('');
+        </div>`,
+  )
+  .join('');
 
-  imageGallery.insertAdjacentHTML('beforeend', markup);
-
+imageGallery.insertAdjacentHTML('beforeend', markup);
 
 console.log(imageGallery);
 
 imageGallery.addEventListener('click', onClick);
 
 function onClick(event) {
-    event.preventDefault()
+  event.preventDefault();
 
   if (!event.target.classList.contains('gallery__image')) {
     return;
   }
   console.log(event.target);
-//   const instance = basicLightbox.create(`
-// 	  <img src="url" width="800" height="600">
-// `);
-//   instance.show();
-    const instance = basicLightbox.create(`
-    <div class="modal">
-         <img src="https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg">
-    </div>
-`)
 
-instance.show()
+  const instance = basicLightbox.create(`
+    <div class="modal">
+         <img src="${event.target.dataset.source}">
+    </div>
+`);
+
+  instance.show();
 }
 
-console.log(onClick);
+imageGallery.removeEventListener('keydown', onEscKeyPress);
+
+function onEscKeyPress(event) {
+  const ESC_KEY_CODE = 'Escape';
+  const isEscKey = event.code === ESC_KEY_CODE;
+
+  if (isEscKey) {
+    instance.close();
+  }
+}

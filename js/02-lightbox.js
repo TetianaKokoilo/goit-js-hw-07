152aaf635ae1ee4d9863adfa047cfb72875deca6
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2022-12-21 17:00:31 +0200

    add task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,29 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const newGallery = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(
+    item =>
+      `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`,
+  )
+  .join('');
+
+newGallery.insertAdjacentHTML('beforeend', markup);
+
+console.log(newGallery);
+
+newGallery.addEventListener('click', onClick);
+
+function onClick(event) {
+    event.preventDefault()
+
+    if (!event.target.classList.contains('gallery__image')) {
+        return;
+    }
+    console.log(event.target);
+}

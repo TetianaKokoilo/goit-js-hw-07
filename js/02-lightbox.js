ed957bd57d5c25d1ab93a8da2aa20efb2b707461
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2022-12-25 13:09:35 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -8,9 +8,9 @@ const newGallery = document.querySelector('.gallery');
 const markup = galleryItems
   .map(
     item =>
-      `<a class="gallery__item" href="${item.original}">
+      `<li><a class="gallery__item" href="${item.original}">
   <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
-</a>`,
+</a></li>`,
   )
   .join('');
 
